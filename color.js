58b2fde9a30476fba1bfd111afe36726b54a4e59
Roshan Pandit <96768343+roshan-pandit1@users.noreply.github.com>
2024-12-16 09:47:11 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,24 @@
+btn=document.querySelector('button')
+
+btn.addEventListener("click",function()
+{
+    let h3=document.querySelector("h3")
+    let randomcolor=generate_color();
+    h3.innerText =randomColor;
+
+    let div=document.querySelector("div")
+    div.style.backgroundColor =randomcolor;
+
+    console.log("color updated")
+})
+
+function generate_color() {
+ 
+    let red =Math.floor(Math.random()*255)
+    let blue =Math.floor(Math.random()*255)
+    let green=Math.floor(Math.random()*255)
+
+    let color= `rgb(${red},${blue},${green})`;
+    return color;
+    
+}
